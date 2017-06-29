a67815d03ec4c06394176bd5a2aec0bdb24ef91c
Fangshu Ruan <ssyz@luboshi1.ssyzedu.net>
2017-06-29 17:57:53 +0800

    ruanfangshu

---
+++ cppex/utility/insertionsort.h
@@ -0,0 +1,17 @@
+#ifndef _INSERTIONSORT_H
+#define _INSERTIONSORT_H
+#include<iterator>
+using namespace std;
+void insertionSort(int *a, int n){
+	int i,j,key;
+	for(j=1;j<n;j++){
+		key=a[j];
+		i=j-1;
+		while(i>=0 && a[i]>key){
+			a[i+1]=a[i];
+			i--;
+		}
+		a[i+1]=a[i];
+	}
+}
+#endif
