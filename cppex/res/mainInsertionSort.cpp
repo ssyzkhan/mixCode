3fc1de1fdced837106b80f1fb6d729d2c4520415
Fangshu Ruan <ssyz@luboshi1.ssyzedu.net>
2017-06-29 16:13:40 +0800

    ruan

---
+++ cppex/res/mainInsertionSort.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<list>
+#include<iterator>
+using namespace std;
+//#include"insertionsort.h"
+int main(int argc, char** argv){
+	int a[]={5,1,9,4,6,2,0,3,8,7},i;
+	for(i=0;i<10;i++){
+		cout<<a[i]<<" ";
+	}
+	cout<<endl;
+	
+}
