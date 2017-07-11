b8957256af44ee20078e291deda1ad8fe0a3a143
Field Ruan <1990029@qq.com>
2017-07-11 22:22:03 +0800

    mainQSort test

---
+++ cex/res/mainQSort.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int a[]={5,1,9,4,6,2,0,3,8,7},i;
+	char *b[]={"ChongQing","ShangHai","AoMen","TianJin","BeiJing","XiangGang"};
+	double c[]={8.5,6.3,1.7,9.2,0.5,2.3,4.1,7.4,5.9,3.7};
+	
+	
+}
